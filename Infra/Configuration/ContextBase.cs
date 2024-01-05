5a74b2799971b93703a845b9dda7cd06761cc81a
Luiz Gonzaga <luizgonzaga15@gmail.com>
2024-01-04 23:33:50 -0300

    Criando a classe de contexto

---
+++ Infra/Configuration/ContextBase.cs
@@ -0,0 +1,27 @@
+﻿using Api.Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options) { }
+
+        public DbSet<Imobile> Imobile { get; set; }
+        public DbSet<Address> Address { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
