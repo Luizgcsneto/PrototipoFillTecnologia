0fa158bd97d5c9f995d207592f0807a93f5250ca
Luiz Gonzaga <luizgonzaga15@gmail.com>
2024-01-04 22:26:02 -0300

    Criação das primeiras classes

---
+++ Api.Entities/Entities/Base.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Entities.Entities
+{
+    public class Base
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+}
